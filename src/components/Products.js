8a8445d2341b9f0103208d42ce651e45fda5c95f
Abhishek Nayak <abhishek.nayak7766@gmail.com>
2024-05-14 23:55:52 +0530

    Revert "error fix"
    This reverts commit afaa256c2ed1f878bd3378b1234a83ea8dd967ce.

--- src/components/Products.js
+++ src/components/Products.js
@@ -47,23 +47,26 @@ const Products = ({ data, type }) => {
     }
   };
 
+  const handelWishList = (item) => {
+    dispatch(AddToWishlist(item));
+  };
   const handleClick = () => {
     window.scrollTo({ top: 0, behavior: "smooth" }); // Scrolls to the top of the page smoothly
   };
   return (
     <>
-      <div
+      <NavLink
         // onClick={() => handleClick()}
         className="group shadow  border overflow-hidden  w-[48%] sm:w-[280px] rounded-md   hover:shadow-lg cursor-pointer h-fit "
       >
         <div className=" relative w-full flex flex-col justify-end   items-center rounded-md h-fit sm:h-fit">
-          <NavLink to={`/products/${data.slug}`} className="  w-fit h-fit">
+          <div className="  w-fit h-fit">
             <img
               src={data.images[0]}
               className=" p-7  sm:p-2 relative transition-all duration-150 easy-linear sm:rounded-md sm:group-hover:w-fit group-hover:rounded-none"
               alt=""
             />
-          </NavLink>
+          </div>
           <div className=" absolute flex justify-between items-center sm:px-2 pt-0 sm:pt-4 top-3 sm:top-5 left-0 h-fit w-full ">
             {type === "New" ? (
               <div
@@ -119,7 +122,7 @@ const Products = ({ data, type }) => {
             </div>
           </div>
         </div>
-      </div>
+      </NavLink>
     </>
   );
 };
